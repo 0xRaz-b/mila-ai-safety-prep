ab8801b66b97f78546a40db11a9e83f5a4f28804
0xRaz-b <razine.bouache@gmail.com>
2026-04-24 10:28:36 -0400

    Adding file for the new challenge

---
+++ sql/sql-practice/solutions/sqlZoo_nobel.sql
@@ -0,0 +1,7 @@
+-- ============================================================
+-- SQLZoo Practice Solutions
+-- Author: [Razine Bouache]
+-- Tables used: world (name, continent, area, population, gdp)
+--              nobel (yr, subject, winner)
+-- ============================================================
+
