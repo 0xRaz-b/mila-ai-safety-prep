d023a2d48d4ba7093e48e7ed1ba307c7cc3e9cbb
0xRaz-b <razine.bouache@gmail.com>
2026-04-24 11:26:00 -0400

    Add query for medicine winners before 1910 and literature winners from 2004

--- sql/sql-practice/solutions/sqlZoo_nobel.sql
+++ sql/sql-practice/solutions/sqlZoo_nobel.sql
@@ -76,3 +76,12 @@ FROM nobel
 WHERE yr = 1980 
 AND subject <> 'chemistry'
 AND subject <> 'medicine' 
+
+-- Q10 Early Medicine, Late Literature
+-- Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910) together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+
+SELECT * 
+FROM nobel
+WHERE (subject ='Medicine'
+AND yr < 1910)
+OR (subject = 'Literature' and yr >= 2004)
