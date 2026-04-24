c1c083dcd56f11e51376308ff16d0e85e7f73541
0xRaz-b <razine.bouache@gmail.com>
2026-04-24 10:38:03 -0400

    challenge 2 finding the price of literature

--- sql/sql-practice/solutions/sqlZoo_nobel.sql
+++ sql/sql-practice/solutions/sqlZoo_nobel.sql
@@ -5,3 +5,17 @@
 -- Link to the tab : https://sqlzoo.net/wiki/SELECT_from_Nobel_Tutorial          
 -- ============================================================
 
+-- Q1: Winners from 1950
+-- Get the all nobel prices for that year 
+
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1950
+
+-- Q1: 1962 Literature
+-- Find the price of literature for a certain year 
+
+SELECT winner
+FROM nobel
+WHERE yr = 1962
+AND subject = 'literature'
