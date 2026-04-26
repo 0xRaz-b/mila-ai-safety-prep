158b9b7f3e726be9b88299624459f284a4b8f3f1
0xRaz-b <razine.bouache@gmail.com>
2026-04-26 19:06:49 -0400

    add: SELECT in SELECT - Europe countries with GDP per capita > UK

--- sql/sql-practice/solutions/sqlZoo_subquery.sql
+++ sql/sql-practice/solutions/sqlZoo_subquery.sql
@@ -12,3 +12,11 @@ SELECT name FROM world
   WHERE population >
      (SELECT population FROM world
       WHERE name='Russia')
+
+-- Q2: Richer than UK
+-- Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
+
+SELECT name
+FROM world
+WHERE continent = 'Europe' 
+AND gdp/population > ( SELECT gdp/population FROM world WHERE name = 'United Kingdom')
