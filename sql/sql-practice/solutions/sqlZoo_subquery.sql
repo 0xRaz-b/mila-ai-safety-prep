d3bdf8b843c5edd2985db107598b7126c7645634
0xRaz-b <razine.bouache@gmail.com>
2026-04-27 21:34:44 -0400

    Add SQLZoo solutions - SELECT World ALL keyword

--- sql/sql-practice/solutions/sqlZoo_subquery.sql
+++ sql/sql-practice/solutions/sqlZoo_subquery.sql
@@ -76,3 +76,14 @@ SELECT continent, name FROM world x
 WHERE name <= ALL
     (SELECT name FROM world y
         WHERE y.continent = x.continent)
+
+-- Q0 Difficult Questions That Utilize Techniques Not Covered In Prior Sections
+-- Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. Show name, continent and population.
+
+SELECT name, continent, population FROM world
+WHERE continent IN (
+    SELECT continent FROM world x
+    WHERE 25000000 >= ALL
+        (SELECT population FROM world y
+            WHERE y.continent = x.continent)
+)
