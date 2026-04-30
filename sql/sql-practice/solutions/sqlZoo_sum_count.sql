7e74621758aa45ed9e50ba75bcc3f7a5b5e3238b
0xRaz-b <razine.bouache@gmail.com>
2026-04-29 21:16:43 -0400

    Showing num of country per continent

--- sql/sql-practice/solutions/sqlZoo_sum_count.sql
+++ sql/sql-practice/solutions/sqlZoo_sum_count.sql
@@ -45,3 +45,10 @@ SELECT continent, COUNT(name)
 FROM world
 GROUP BY continent
 
+-- Q7: Counting big countries in each continent
+-- For each continent show the continent and number of countries with populations of at least 10 million.
+
+SELECT continent, COUNT(name)
+FROM world
+WHERE population > 10000000
+GROUP BY continent
