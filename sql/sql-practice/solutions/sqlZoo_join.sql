39a946d5dba624dd92b703f0c912f5ea737f0bd7
0xRaz-b <razine.bouache@gmail.com>
2026-05-04 09:26:30 -0400

    add GROUP BY query to count goals in matches involving Poland

--- sql/sql-practice/solutions/sqlZoo_join.sql
+++ sql/sql-practice/solutions/sqlZoo_join.sql
@@ -83,3 +83,11 @@ GROUP BY teamname
 SELECT stadium, COUNT(*)
 FROM game JOIN goal on (goal.matchid = game.id)
 GROUP BY stadium
+
+-- Q11: Polish goals
+-- For every match involving 'POL', show the matchid, date and the number of goals scored.
+
+SELECT matchid,mdate, COUNT(*)
+FROM game JOIN goal ON (matchid = id)
+WHERE (team1 = 'POL' OR team2 = 'POL')
+GROUP BY matchid,mdate
